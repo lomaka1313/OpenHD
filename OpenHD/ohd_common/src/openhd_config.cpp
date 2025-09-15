4e031694efdfb682a39a166fa44dd339943aa801
i.shtern <i.shtern@mil.ua>
2025-09-15 08:48:16 +0300

    upd

--- OpenHD/ohd_common/src/openhd_config.cpp
+++ OpenHD/ohd_common/src/openhd_config.cpp
@@ -9,7 +9,7 @@
 
 #include "../lib/ini/ini.hpp"
 
-static std::string CONFIG_FILE_PATH="/boot/openhd/hardware.config";
+static std::string CONFIG_FILE_PATH="/usr/local/share/openhd/hardware.config";
 
 static std::shared_ptr<spdlog::logger> get_logger(){
   return openhd::log::create_or_get("config");
@@ -43,6 +43,7 @@ static openhd::Config load_or_default(){
       ret.NW_FORWARD_TO_LOCALHOST_58XX = r.Get<bool>("network","NW_FORWARD_TO_LOCALHOST_58XX");
 
       // Parse Ethernet link configuration
+      ret.ETH_LINK_ENABLED = r.Get<bool>("ethernet", "ETH_LINK_ENABLED", false);
       ret.ETH_GROUND_UNIT_IP = r.Get<std::string>("ethernet", "ETH_GROUND_UNIT_IP", "");
       ret.ETH_AIR_UNIT_IP = r.Get<std::string>("ethernet", "ETH_AIR_UNIT_IP", "");
       ret.ETH_VIDEO_PORT =

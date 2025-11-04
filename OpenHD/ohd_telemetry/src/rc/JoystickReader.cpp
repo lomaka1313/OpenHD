36b810b1a78f0204bb2ff78a2f87b2b611a8fad1
i.shtern <i.shtern@mil.ua>
2025-11-04 16:40:36 +0200

    upd joistick

--- OpenHD/ohd_telemetry/src/rc/JoystickReader.cpp
+++ OpenHD/ohd_telemetry/src/rc/JoystickReader.cpp
@@ -4,46 +4,47 @@
 #include "JoystickReader.h"
 
 #include <SDL2/SDL.h>
-
-#include <iostream>
 #include <unistd.h>
+
+#include <cassert>
+#include <iostream>
 #include <sstream>
 
-static constexpr auto JOYSTICK_N=0;
+static constexpr auto JOYSTICK_N = 0;
 /*static constexpr auto JOY_DEV="/sys/class/input/js0";
 static bool check_if_joystick_is_connected_via_fd(){
   return access(JOY_DEV, F_OK);
 }*/
 
-static SDL_Joystick *js;
+static SDL_Joystick* js;
 
-// SDL documentation: Joystick axis values are in the range (-32768 to 32767) and of type Sint16 (int16_t )
-// Mavlink wants uint16_t and in the range [1000-2000]
+// SDL documentation: Joystick axis values are in the range (-32768 to 32767)
+// and of type Sint16 (int16_t ) Mavlink wants uint16_t and in the range
+// [1000-2000]
 static uint16_t remap_sdl_to_mavlink(int16_t value) {
-    return (int16_t)(((((double)value)+32768.0)/65.536)+1000);
+  return (int16_t)(((((double)value) + 32768.0) / 65.536) + 1000);
 }
 
 JoystickReader::JoystickReader() {
   m_console = openhd::log::create_or_get("joystick_reader");
   assert(m_console);
-  // WARNING: Joystick logging is a bit different than the rest regarding log level
-  //m_console->set_level(spdlog::level::warn);
+  // WARNING: Joystick logging is a bit different than the rest regarding log
+  // level
+  // m_console->set_level(spdlog::level::warn);
   m_console->debug("JoystickReader::JoystickReader");
   reset_curr_values();
-  m_read_joystick_thread=std::make_unique<std::thread>([this] {
-    loop();
-  });
+  m_read_joystick_thread = std::make_unique<std::thread>([this] { loop(); });
 }
 
 JoystickReader::~JoystickReader() {
   m_console->debug("JoystickReader::~JoystickReader()");
-  terminate= true;
+  terminate = true;
   m_read_joystick_thread->join();
-  m_read_joystick_thread= nullptr;
+  m_read_joystick_thread = nullptr;
 }
 
 void JoystickReader::loop() {
-  while (!terminate){
+  while (!terminate) {
     connect_once_and_read_until_error();
     // Error / no joystick found, try again later
     std::this_thread::sleep_for(std::chrono::seconds(3));
@@ -51,81 +52,86 @@ void JoystickReader::loop() {
 }
 
 void JoystickReader::connect_once_and_read_until_error() {
-  // We only need SDL "Joy", and are not interested in any display output or similar.
+  // We only need SDL "Joy", and are not interested in any display output or
+  // similar.
   /*if (SDL_Init (SDL_INIT_JOYSTICK | SDL_INIT_VIDEO) != 0){
     m_console->warn("SDL_INIT Error: {}",SDL_GetError());
     return;
   }*/
   if (SDL_InitSubSystem(SDL_INIT_GAMECONTROLLER | SDL_INIT_JOYSTICK) < 0) {
-    m_console->warn("SDL_INIT_SubSystem Error: {}",SDL_GetError());
+    m_console->warn("SDL_INIT_SubSystem Error: {}", SDL_GetError());
     return;
   }
   SDL_JoystickEventState(SDL_ENABLE);
-  const auto n_joysticks=SDL_NumJoysticks();
-  if(n_joysticks<1){
-    m_console->warn("No joysticks, num:{}",n_joysticks);
+  const auto n_joysticks = SDL_NumJoysticks();
+  if (n_joysticks < 1) {
+    m_console->warn("No joysticks, num:{}", n_joysticks);
     SDL_QuitSubSystem(SDL_INIT_GAMECONTROLLER | SDL_INIT_JOYSTICK);
     SDL_Quit();
     return;
   }
-  m_console->debug("N joysticks: {}",n_joysticks);
+  m_console->debug("N joysticks: {}", n_joysticks);
   js = SDL_JoystickOpen(JOYSTICK_N);
-  if (js == nullptr){
-    m_console->warn("Couldn't open desired Joystick: {}",SDL_GetError());
+  if (js == nullptr) {
+    m_console->warn("Couldn't open desired Joystick: {}", SDL_GetError());
     SDL_QuitSubSystem(SDL_INIT_GAMECONTROLLER | SDL_INIT_JOYSTICK);
     SDL_Quit();
     return;
   }
-  auto name_pointer=SDL_JoystickName(js);
-  std::string name="unknown";
-  if(name_pointer!= nullptr){
-    name=std::string(name_pointer);
+  auto name_pointer = SDL_JoystickName(js);
+  std::string name = "unknown";
+  if (name_pointer != nullptr) {
+    name = std::string(name_pointer);
   }
   std::stringstream ss;
-  ss<<"Found joystick:\n";
-  ss<<"Name:"<<name<<"\n";
-  ss<<"N Axis:"<<SDL_JoystickNumAxes(js)<<"\n";
-  ss<<"Trackballs::"<<SDL_JoystickNumBalls(js)<<"\n";
-  ss<<"Buttons:"<<SDL_JoystickNumButtons(js)<<"\n";
-  ss<<"Hats:"<<SDL_JoystickNumHats(js)<<"\n";
+  ss << "Found joystick:\n";
+  ss << "Name:" << name << "\n";
+  ss << "N Axis:" << SDL_JoystickNumAxes(js) << "\n";
+  ss << "Trackballs::" << SDL_JoystickNumBalls(js) << "\n";
+  ss << "Buttons:" << SDL_JoystickNumButtons(js) << "\n";
+  ss << "Hats:" << SDL_JoystickNumHats(js) << "\n";
   m_console->info(ss.str());
-  // Populate the data once by querying everything (after that, we just get the events from SDL)
+  // Populate the data once by querying everything (after that, we just get the
+  // events from SDL)
   {
     // make a copy
-    auto copy=m_curr_values;
-    for(int i=0;i< SDL_JoystickNumAxes(js);i++){
-      const auto curr= SDL_JoystickGetAxis(js,i);
-      write_matching_axis(copy.values,i,curr);
+    auto copy = m_curr_values;
+    for (int i = 0; i < SDL_JoystickNumAxes(js); i++) {
+      const auto curr = SDL_JoystickGetAxis(js, i);
+      write_matching_axis(copy.values, i, curr);
     }
-    for(int i=0;i< SDL_JoystickNumButtons(js);i++){
-      const auto curr= SDL_JoystickGetButton(js,i);
-      write_matching_button(copy.values,i,curr==0);
+    for (int i = 0; i < SDL_JoystickNumButtons(js); i++) {
+      const auto curr = SDL_JoystickGetButton(js, i);
+      write_matching_button(copy.values, i, curr == 0);
     }
     // write out the results
     std::lock_guard<std::mutex> guard(m_curr_values_mutex);
-    m_curr_values=copy;
-    m_curr_values.considered_connected= true;
-    m_curr_values.last_update=std::chrono::steady_clock::now();
-    m_curr_values.joystick_name=name;
+    m_curr_values = copy;
+    m_curr_values.considered_connected = true;
+    m_curr_values.last_update = std::chrono::steady_clock::now();
+    m_curr_values.joystick_name = name;
   }
-  // We constantly check for a disconnected joystick, in which case we set the joystick state to disconnected
-  // and return.
-  bool disconnected=false;
-  while (!disconnected && !terminate){
+  // We constantly check for a disconnected joystick, in which case we set the
+  // joystick state to disconnected and return.
+  bool disconnected = false;
+  while (!disconnected && !terminate) {
     wait_for_events(200);
-    const int curr_num_joysticks=SDL_NumJoysticks();
-    if(curr_num_joysticks<1){
+    const int curr_num_joysticks = SDL_NumJoysticks();
+    if (curr_num_joysticks < 1) {
       // This one seems to work to check if the joystick disconnected
-      m_console->warn("Joystick disconnected, SDL_NumJoysticks:{}",curr_num_joysticks);
-      disconnected= true;
+      m_console->warn("Joystick disconnected, SDL_NumJoysticks:{}",
+                      curr_num_joysticks);
+      disconnected = true;
     }
-    if(!SDL_JoystickGetAttached(js)){
-      m_console->warn("Joystick disconnected, SDL_JoystickGetAttached() reports false");
-      disconnected= true;
+    if (!SDL_JoystickGetAttached(js)) {
+      m_console->warn(
+          "Joystick disconnected, SDL_JoystickGetAttached() reports false");
+      disconnected = true;
     }
   }
   m_console->info("Joystick disconnected");
-  // This will set considered_connected to false, such that we don't send obsolete updates
+  // This will set considered_connected to false, such that we don't send
+  // obsolete updates
   reset_curr_values();
   SDL_QuitSubSystem(SDL_INIT_GAMECONTROLLER | SDL_INIT_JOYSTICK);
   SDL_Quit();
@@ -133,75 +139,79 @@ void JoystickReader::connect_once_and_read_until_error() {
 }
 
 void JoystickReader::wait_for_events(const int timeout_ms) {
-  auto current=m_curr_values.values;
-  int n_polled_events=0;
+  auto current = m_curr_values.values;
+  int n_polled_events = 0;
   SDL_Event event;
-  bool any_new_data=false;
+  bool any_new_data = false;
   // wait for at least one event with a timeut
-  if(!SDL_WaitEventTimeout(&event,timeout_ms)){
-    //m_console->debug("Got no event after 100ms");
+  if (!SDL_WaitEventTimeout(&event, timeout_ms)) {
+    // m_console->debug("Got no event after 100ms");
     return;
   }
   // process this event
-  auto ret= process_event(&event,current);
-  if(ret==2 || ret==5 || ret==4){
-    any_new_data= true;
+  auto ret = process_event(&event, current);
+  if (ret == 2 || ret == 5 || ret == 4) {
+    any_new_data = true;
     n_polled_events++;
   }
-  if(ret==-1){
+  if (ret == -1) {
     // Terminate the Joystick reader, such that SDL is terminated
-    m_console->warn("Terminating joystick reader, it won't restart until openhd is restarted");
-    terminate=true;
-    return ;
+    m_console->warn(
+        "Terminating joystick reader, it won't restart until openhd is "
+        "restarted");
+    terminate = true;
+    return;
   }
-  // and then get as many more events as we can get (we already spun up the thread anyways)
-  while (SDL_PollEvent (&event)) {
-    ret= process_event(&event,current);
-    if(ret==2 || ret==5 || ret==4){
-      any_new_data= true;
+  // and then get as many more events as we can get (we already spun up the
+  // thread anyways)
+  while (SDL_PollEvent(&event)) {
+    ret = process_event(&event, current);
+    if (ret == 2 || ret == 5 || ret == 4) {
+      any_new_data = true;
       n_polled_events++;
     }
   }
-  //m_console->debug("N polled events:{}",n_polled_events);
-  if(any_new_data){
+  // m_console->debug("N polled events:{}",n_polled_events);
+  if (any_new_data) {
     std::lock_guard<std::mutex> guard(m_curr_values_mutex);
-    for(int i=0;i<m_curr_values.values.size();i++){
-      m_curr_values.values[i]=current[i];
+    for (int i = 0; i < m_curr_values.values.size(); i++) {
+      m_curr_values.values[i] = current[i];
     }
-    m_curr_values.last_update=std::chrono::steady_clock::now();
-    m_curr_values.considered_connected= true;
+    m_curr_values.last_update = std::chrono::steady_clock::now();
+    m_curr_values.considered_connected = true;
   }
 }
 
-
-int JoystickReader::process_event(void *event1,std::array<uint16_t,N_CHANNELS>& current) {
-  auto* event=(SDL_Event*)event1;
-  int ret=0;
-  m_console->debug("Joystick event: [{}]", event->type);
+int JoystickReader::process_event(void* event1,
+                                  std::array<uint16_t, N_CHANNELS>& current) {
+  auto* event = (SDL_Event*)event1;
+  int ret = 0;
   switch (event->type) {
     case SDL_JOYAXISMOTION:
-      m_console->debug("Joystick {}, Axis {} moved to {}", event->jaxis.which, event->jaxis.axis, event->jaxis.value);
-      write_matching_axis(current, event->jaxis.axis,event->jaxis.value);
-      ret= 2;
+      write_matching_axis(current, event->jaxis.axis, event->jaxis.value);
+      m_console->debug("Joystick {}, Axis {} moved to {}, mavlink value {}", event->jaxis.which,
+                       event->jaxis.axis, event->jaxis.value, remap_sdl_to_mavlink(event->jaxis.value));
+      ret = 2;
       break;
     case SDL_JOYBUTTONDOWN:
-      m_console->debug("Button {} down", event->jbutton.button);
-      write_matching_button(current, event->jbutton.button, true);
-      ret=5;
+      m_console->debug("Button down");
+      write_matching_button(current, event->jbutton.button, false);
+      ret = 5;
       break;
     case SDL_JOYBUTTONUP:
-      m_console->debug("Button {} up", event->jbutton.button);
-      write_matching_button(current, event->jbutton.button, false);
-      ret=4;
+      m_console->debug("Button up");
+      write_matching_button(current, event->jbutton.button, true);
+      ret = 4;
       break;
     case SDL_QUIT:
       m_console->debug("Got SDL_QUIT");
-      ret=-1;
+      ret = -1;
       break;
     default:
-      // aparently we sometimes also get mouse / keyboard events from SDL, this is not an issue
+      // aparently we sometimes also get mouse / keyboard events from SDL, this
+      // is not an issue
       m_console->debug("Got Unknown SDL event type");
-      ret=0;
+      ret = 0;
       break;
   }
   return ret;
@@ -214,47 +224,87 @@ JoystickReader::CurrChannelValues JoystickReader::get_current_state() {
 
 void JoystickReader::reset_curr_values() {
   std::lock_guard<std::mutex> guard(m_curr_values_mutex);
-  m_curr_values.considered_connected=false;
-  for(auto& el:m_curr_values.values){
-    el=DEFAULT_RC_CHANNELS_VALUE;
+  m_curr_values.considered_connected = false;
+  for (auto& el : m_curr_values.values) {
+    el = DEFAULT_RC_CHANNELS_VALUE;
   }
-  m_curr_values.joystick_name="unknown";
+  m_curr_values.joystick_name = "unknown";
 }
 
 std::string JoystickReader::curr_state_to_string(
     const JoystickReader::CurrChannelValues& curr_channel_values) {
   std::stringstream ss;
-  ss<<"Connected:"<<(curr_channel_values.considered_connected ? "Y":"N")<<"\n";
-  ss<<"Name:"<<curr_channel_values.joystick_name<<"\n";
-  ss<<"Values:[";
-  for(int i=0;i<curr_channel_values.values.size();i++){
-    ss<<(int)curr_channel_values.values[i];
-    if(i==curr_channel_values.values.size()-1){
-      ss<<"]\n";
-    }else{
-      ss<<",";
+  ss << "Connected:" << (curr_channel_values.considered_connected ? "Y" : "N")
+     << "\n";
+  ss << "Name:" << curr_channel_values.joystick_name << "\n";
+  ss << "Values:[";
+  for (int i = 0; i < curr_channel_values.values.size(); i++) {
+    ss << (int)curr_channel_values.values[i];
+    if (i == curr_channel_values.values.size() - 1) {
+      ss << "]\n";
+    } else {
+      ss << ",";
     }
   }
-  const auto delay_since_last_update=std::chrono::steady_clock::now()-curr_channel_values.last_update;
-  ss<<"Delay since last update:"<<std::chrono::duration_cast<std::chrono::milliseconds>(delay_since_last_update).count()<<"ms";
+  const auto delay_since_last_update =
+      std::chrono::steady_clock::now() - curr_channel_values.last_update;
+  ss << "Delay since last update:"
+     << std::chrono::duration_cast<std::chrono::milliseconds>(
+            delay_since_last_update)
+            .count()
+     << "ms";
   return ss.str();
 }
 
-void JoystickReader::write_matching_axis(std::array<uint16_t, JoystickReader::N_CHANNELS>& rc_data,const uint8_t axis_index, const Sint16 value) {
-  if(axis_index>=N_CHANNELS_RESERVED_FOR_AXES){
-    m_console->warn("only {} channels reserved for axis, wanted {}",N_CHANNELS_RESERVED_FOR_AXES,axis_index);
+void JoystickReader::write_matching_axis(
+    std::array<uint16_t, JoystickReader::N_CHANNELS>& rc_data,
+    const uint8_t axis_index, const Sint16 value) {
+  if (axis_index >= N_CHANNELS_RESERVED_FOR_AXES + N_CHANNELS_RESERVED_FOR_SLIDERS) {
+    m_console->warn("only {} channels reserved for axis, wanted {}",
+                    N_CHANNELS_RESERVED_FOR_AXES, axis_index);
     return;
   }
-  rc_data[axis_index]=remap_sdl_to_mavlink(value);
+
+  if (axis_index >= N_CHANNELS_RESERVED_FOR_AXES) {
+    rc_data[axis_index + N_CHANNELS_RESERVED_FOR_BUTTONS] = remap_sdl_to_mavlink(value);
+    return;
+  }
+  rc_data[axis_index] = remap_sdl_to_mavlink(value);
 }
 
-void JoystickReader::write_matching_button(std::array<uint16_t, 18>& rc_data,const Uint8 button, bool pressed) {
-  // The mavlink rc channels override message has more than enough "channels" anyways.
-  //However, we could optimize here putting multiple buttons (aka bool) into one channel
-  // const auto map = buttons_map.at(button);
-
-  const int channel_index=JoystickReader::N_CHANNELS_RESERVED_FOR_AXES+button;
-  if(channel_index<rc_data.size()){
-    rc_data[channel_index] = pressed ? JoystickReader::VALUE_BUTTON_UP : JoystickReader::VALUE_BUTTON_DOWN;
+void JoystickReader::write_matching_button(std::array<uint16_t, N_CHANNELS>& rc_data,
+                                           const uint8_t button, bool up) {
+  bool pressed = !up;
+  // якщо стабільний стан, але змінився — оновлюємо
+  if (button_states[button] != pressed) {
+    button_states[button] = pressed;
+    for (const auto& [sw_id, sw] : switches) {
+      if (std::find(sw.buttons.begin(), sw.buttons.end(), button) != sw.buttons.end()) {
+        rc_data[sw.channel - 1] = calc_pwm_for_switch(sw);
+      }
+    }
   }
+}
+
+int JoystickReader::calc_pwm_for_switch(const SwitchConfig& sw) {
+  if (sw.type == SwitchType::TWO_POS) {
+    bool state = button_states[sw.buttons[0]];
+    return state ? JoystickReader::VALUE_BUTTON_UP : JoystickReader::VALUE_BUTTON_DOWN;
+  }
+
+  if (sw.type == SwitchType::THREE_POS) {
+    for (size_t i = 0; i < sw.buttons.size(); ++i) {
+      if (button_states[sw.buttons[i]]) {
+        switch (i) {
+          case 0: return JoystickReader::VALUE_BUTTON_DOWN;
+          case 1: return JoystickReader::VALUE_BUTTON_MIDDLE;
+          case 2: return JoystickReader::VALUE_BUTTON_UP;
+        }
+      }
+    }
+
+    return JoystickReader::VALUE_BUTTON_MIDDLE;
+  }
+
+  return JoystickReader::VALUE_BUTTON_MIDDLE;
 }
--- OpenHD/ohd_telemetry/src/rc/JoystickReader.h
+++ OpenHD/ohd_telemetry/src/rc/JoystickReader.h
@@ -7,7 +7,6 @@
 #include <functional>
 #include <map>
 #include <mutex>
-#include <optional>
 #include <sstream>
 #include <thread>
 
@@ -19,65 +18,92 @@
  * The Paradigm of this class is similar to how for example external devices
  * are handled in general in OpenHD: If the user says he wants RC joystick
  * control, try to open the joystick and read data, re-connect if anything goes
- * wrong during run time. This class does all the connecting and handles disconnecting
- * and reading values in its own thread - you can query a "state" from any thread at any
- * time though.
- * Theoretically, we could just use this thread also for sending the RC data via mavlink - but this
- * is a bit dangerous, since I don't completely trust SDL yet (in regards to disconnecting joysticks).
+ * wrong during run time. This class does all the connecting and handles
+ * disconnecting and reading values in its own thread - you can query a "state"
+ * from any thread at any time though. Theoretically, we could just use this
+ * thread also for sending the RC data via mavlink - but this is a bit
+ * dangerous, since I don't completely trust SDL yet (in regards to
+ * disconnecting joysticks).
 */
-struct BUTTONS_TO_CHANNEL_MAP {
-  std::array<uint16_t,3> dependency{};
-  uint16_t channel{};
-  bool single;
+
+enum class SwitchType {
+  TWO_POS,
+  THREE_POS
 };
-static std::map<uint8_t,BUTTONS_TO_CHANNEL_MAP> buttons_map = std::map<uint8_t,BUTTONS_TO_CHANNEL_MAP>{
-  {0, {{0}, 5, true}},
-  {1, {{2,3}, 6}},
-  {2, {{1,3}, 6}},
-  {3, {{1,2}, 6}},
-  {4, {{4,5,6}, 7}},
+
+struct SwitchConfig {
+  SwitchType type;              // тип тумблера
+  std::vector<uint8_t> buttons; // кнопки, які його формують
+  uint16_t channel;             // канал PWM
 };
+
+// Конфігурація тумблерів
+static std::map<uint8_t, SwitchConfig> switches = {
+  {0, {SwitchType::TWO_POS,   {0},        5}},
+  {1, {SwitchType::THREE_POS, {1, 2, 3},  6}},
+  {3, {SwitchType::THREE_POS, {4, 5, 6},  7}},
+  {4, {SwitchType::THREE_POS, {7, 8, 9},  8}},
+  {5, {SwitchType::THREE_POS, {10, 11, 12},  9}},
+  {6, {SwitchType::THREE_POS, {13, 14, 15},  10}},
+  {7, {SwitchType::THREE_POS, {16, 17, 18},  11}},
+  {8, {SwitchType::TWO_POS, {19},  12}},
+};
+
+
+// Стан кнопок
+static std::map<uint8_t, bool> button_states;   // стабільний стан
+
 class JoystickReader {
  public:
-  // See mavlink RC override https://mavlink.io/en/messages/common.html#RC_CHANNELS_OVERRIDE
-  static constexpr uint16_t DEFAULT_RC_CHANNELS_VALUE=UINT16_MAX;
+  // See mavlink RC override
+  // https://mavlink.io/en/messages/common.html#RC_CHANNELS_OVERRIDE
+  static constexpr uint16_t DEFAULT_RC_CHANNELS_VALUE = UINT16_MAX;
   // the rc channel override message(s) support 18 values, so we do so, too
-  static constexpr auto N_CHANNELS=18;
+  static constexpr auto N_CHANNELS = 18;
   // We use the first 8 Channels for "axis" joystick values
-  static constexpr auto N_CHANNELS_RESERVED_FOR_AXES=4;
-  static constexpr auto N_CHANNELS_RESERVED_FOR_SLIDERS=4;
-  static constexpr uint16_t VALUE_BUTTON_UP=2000;
-  static constexpr uint16_t VALUE_BUTTON_MIDDLE=1500;
-  static constexpr uint16_t VALUE_BUTTON_DOWN=1000;
-  struct CurrChannelValues{
-    std::array<uint16_t,N_CHANNELS> values{DEFAULT_RC_CHANNELS_VALUE};
-    // Time point when we received the last update to at least one of the channel(s)
+  static constexpr auto N_CHANNELS_RESERVED_FOR_AXES = 4;
+  static constexpr auto N_CHANNELS_RESERVED_FOR_BUTTONS = 8;
+  static constexpr auto N_CHANNELS_RESERVED_FOR_SLIDERS = 4;
+  static constexpr uint16_t VALUE_BUTTON_UP = 2000;
+  static constexpr uint16_t VALUE_BUTTON_MIDDLE = 1500;
+  static constexpr uint16_t VALUE_BUTTON_DOWN = 1000;
+  struct CurrChannelValues {
+    std::array<uint16_t, N_CHANNELS> values{DEFAULT_RC_CHANNELS_VALUE};
+    // Time point when we received the last update to at least one of the
+    // channel(s)
     std::chrono::steady_clock::time_point last_update;
     // Weather we think the RC (joystick) is currently connected or not.
-    bool considered_connected=false;
+    bool considered_connected = false;
     // the name of the joystick
-    std::string joystick_name="unknown";
+    std::string joystick_name = "unknown";
   };
   explicit JoystickReader();
   ~JoystickReader();
   // Get the current "state", thread-safe
   CurrChannelValues get_current_state();
   // For debugging
-  static std::string curr_state_to_string(const CurrChannelValues& curr_channel_values);
+  static std::string curr_state_to_string(
+      const CurrChannelValues& curr_channel_values);
+
  private:
   void loop();
   void connect_once_and_read_until_error();
-  // Wait up to timeout_ms for an event, and then read as many events as there are available
-  // We are only interested in the Joystick events
+  // Wait up to timeout_ms for an event, and then read as many events as there
+  // are available We are only interested in the Joystick events
   void wait_for_events(int timeout_ms);
-  int process_event(void* event,std::array<uint16_t,N_CHANNELS>& values);
+  int process_event(void* event, std::array<uint16_t, N_CHANNELS>& values);
   void reset_curr_values();
   std::unique_ptr<std::thread> m_read_joystick_thread;
-  bool terminate=false;
+  bool terminate = false;
   std::mutex m_curr_values_mutex;
   CurrChannelValues m_curr_values;
   std::shared_ptr<spdlog::logger> m_console;
+
  private:
-  void write_matching_axis(std::array<uint16_t,JoystickReader::N_CHANNELS>& rc_data,uint8_t axis_index,int16_t value);
-  static void write_matching_button(std::array<uint16_t,18>&rc_data,uint8_t button,bool up);
+  void write_matching_axis(
+      std::array<uint16_t, JoystickReader::N_CHANNELS>& rc_data,
+      uint8_t axis_index, int16_t value);
+  static void write_matching_button(std::array<uint16_t, 18>& rc_data,
+                                    uint8_t button, bool up);
+  static int calc_pwm_for_switch(const SwitchConfig& sw);
 };
